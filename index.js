451111b26bfed6a3e105e9d6b0b6fbe5a82f8361
Ashique <ashique12009@gmail.com>
2023-01-22 21:38:48 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const btnEl = document.getElementById("btn");
+const jokeEl = document.getElementById("joke");
+
+const apiKey = "JZer/Y3N884l0+lLi9q6KA==D0hONPu4Wc6VixuR";
+
+const options = {
+    method: "GET",
+    headers: {
+        "X-Api-Key": apiKey
+    }
+}
+
+const limit= 1;
+
+const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=" + limit;
+
+async function getJoke() {
+
+    jokeEl.innerText = "Loading...";
+
+    const response = await fetch(apiURL, options);
+    const data = await response.json();
+
+    jokeEl.innerText = data[0].joke;
+}
+
+btnEl.addEventListener("click", getJoke);
